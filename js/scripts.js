394b151b10e89fccff1e4d49d379cf031d351906
duncan19 <duncanrobbins19@gmail.com>
2020-03-23 11:19:35 -0700

    initial

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var x = 'joe said "hello"'
+var b = 'and waved his hand'
+var Colors = "dark", Color1 ="blue", Color2 = "green"
+window.alert(x)
+document.write(x + " " + b)
+document.write(Color2)
+var green = "this is green"
+var green = green.fontcolor("green")
+document.write(green)
+document.write(3 + 3);
